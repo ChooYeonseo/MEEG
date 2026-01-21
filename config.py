df34acad5c48e9233e2afb752f5640825d06c87b
seans link lab computer <seanchoo03@korea.ac.kr>
2026-01-21 14:11:27 +0900

    change

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ used throughout the application.
 
 # Application metadata
 APP_NAME = "EEG Analysis Tool"
-APP_VERSION = "1.0.0"
+APP_VERSION = "1.0.1"
 GITHUB_REPO = "ChooYeonseo/MEEG"
 ORGANIZATION_NAME = "EEG Research"
 ORGANIZATION_DOMAIN = "eeg-research.org"
